3ea5d8b3e4eea13d8037b7ec32989d3f70264d93
jhonnatanBezerra <jhonnatan.bezerra@estudante.ifms.edu.br>
2020-11-16 22:32:41 -0400

    salvando acoes sem img

--- src/pages/acao/index.js
+++ src/pages/acao/index.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../../components/header';
+import api from '../../services/ApiSwagger';
 
 import { VscNewFile } from 'react-icons/vsc';
 import { IoMdClose } from 'react-icons/io'
@@ -11,6 +12,23 @@ import { Dialog } from 'primereact/dialog';
 export default function Acao() {
 
   const [hidden, setHidden] = useState(false);
+
+  const usuarioID = localStorage.getItem('id');
+  const [listBairros, setListBairros] = useState([]);
+  const [data, setData] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [bairroID, setBairroID] = useState('');
+  const [file, setFile] = useState([]);
+
+
+
+  useEffect(() => {
+
+    api.get('bairros').then(response => {
+      setListBairros(response.data);
+    }, []);
+  }, []);
 
   function openModal() {
     setHidden(true);
@@ -25,7 +43,7 @@ export default function Acao() {
 
       <div className="modal-buttons">
         <button onClick={closeModal}> <IoMdClose />Cancelar</button>
-        <button type="submit" > <FiCheck />Cadastrar</button>
+        <button type="submit" onClick={handleCreate}> <FiCheck />Cadastrar</button>
       </div>
     );
 
@@ -39,18 +57,63 @@ export default function Acao() {
     )
   }
 
+  async function handleCreate(e) {
+    e.preventDefault();
+
+
+
+    const dados = {
+      gestor: {
+        id: usuarioID
+      },
+
+      bairro: {
+        id: bairroID
+      },
+
+      date: data,
+      descricao: description,
+      titulo: title,
+      foto: file,
+
+    };
+
+    console.log(dados.foto);
+
+    // try {
+    //   await api.post('acoes', dados);
+    //   alert('Sucesso');
+    //   // showTost('success', 'Agendamento salvo com sucesso !!!', 'Maravilha !')
+    //   closeModal();
+
+    // } catch (err) {
+    //   alert(err);
+    // }
+
+  }
+
 
   return (
     <>
       <Dialog visible={hidden} header={headerModal()} footer={footerModalButtons()} style={{ width: '45vw' }} onHide={closeModal}>
         <div className="modal-container" >
           <form className="input-dialog">
-            <input type="date" placeholder="" />
-            <input type="text" placeholder="Titulo" />
-            <textarea type="text" placeholder="Descrição" />
-            <select >
+
+            <input type="date" onChange={e => setData(e.target.value)} />
+
+            <select onChange={e => setBairroID(e.target.value)}>
               <option value="">Bairro</option>
+              {listBairros.map(bairro => (
+                <option key={bairro.id} value={bairro.id}>{bairro.nome}</option>
+              ))}
             </select>
+
+            <input type="text" placeholder="Titulo" onChange={e => setTitle(e.target.value)} />
+
+            <textarea type="text" placeholder="Descrição" onChange={e => setDescription(e.target.value)} />
+
+            <input type="file" multiple onChange={e => setFile(e.target.value)} />
+
           </form>
         </div>
       </Dialog>
